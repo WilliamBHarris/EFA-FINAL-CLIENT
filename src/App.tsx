b30b44179e92fb7659ca1f0cbeb65dc67e89116e
Will Harris <bharris010486@gmail.com>
2022-02-12 16:59:57 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -78,32 +78,32 @@ const App = () => {
     updateReview();
   }, [reviewId, revId]);
 
-  // useEffect(() => {
-  //   if (localStorage.getItem('Authorization'))
-  //   setSessionToken(localStorage.getItem('Authorization')); 
+  useEffect(() => {
+    if (localStorage.getItem('Authorization'))
+    setSessionToken(localStorage.getItem('Authorization')); 
 
-  //     const fetchUser = async ():Promise<void> => {
-  //         await fetch(`${dbCall}/user/checkToken`, {
-  //           method: 'POST',
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //             Authorization: `Bearer ${sessionToken}`
-  //           }
-  //         })
-  //         .then(res => {
-  //           console.log('user fetch')
-  //           return res.json()
-  //         })
-  //         .then((res) => {
-  //           setUserId(res.userId)
-  //         })
+      const fetchUser = async ():Promise<void> => {
+          await fetch(`${dbCall}/user/checkToken`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${sessionToken}`
+            }
+          })
+          .then(res => {
+            console.log('user fetch')
+            return res.json()
+          })
+          .then((res) => {
+            setUserId(res.userId)
+          })
   
-  //         .catch(error => console.log(error))
-  //     }
+          .catch(error => console.log(error))
+      }
 
-  //     fetchUser()
+      fetchUser()
 
-  // }, [])
+  }, [])
 
   const updateToken = (newToken: string) => {
     localStorage.setItem("Authorization", newToken);
